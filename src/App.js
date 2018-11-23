6c1b83cf4216807bfdeb790cb76b4e48f6efcdd2
Adrian <adrianvalente1@gmail.com>
2018-11-23 14:33:40 -0200

    teste

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload teste de criação Adrian.
+            Edit <code>src/App.js</code>  criação Adrian.
           </p>
           <a
             className="App-link"
