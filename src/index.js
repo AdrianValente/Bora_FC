30fe7e1dfd4a7a05690e52cac795aa39b14d4198
Adrian <adrianvalente1@gmail.com>
2018-11-28 10:48:52 -0200

    cores

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00695c',
+    },
+    secondary: {
+      main: '#283593',
+    },
+  },
+
+});
+
 ReactDOM.render((
     <BrowserRouter basename={'my-app'}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </BrowserRouter>
   ), document.getElementById('root'))
