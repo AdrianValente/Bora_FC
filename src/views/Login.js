6b5ed69263754f283b56ee7f78a10bbf2199171f
Adrian <adrianvalente1@gmail.com>
2018-12-03 10:25:13 -0200

    home

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 
-class PagHome extends Component {
-  render() {
-    return (
-      <div>
-          Login
-      </div>
-    );
-  }
+import imgUrl from '../img/bg_home.jpg';
+import imgDestaque from '../img/logo_home.png';
+
+const styles = theme => ({
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+  },
+  gbContainerLogin: {
+    backgroundImage: 'url(' + imgUrl + ')',
+    backgroundRepeat  : 'repeat-x',
+    backgroundPosition: 'center',
+    height: '100vh',
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  bigAvatar: {
+    margin: '10px',
+    width: '250px',
+    height: '250px',
+  },
+
+
+});
+
+function Login(props) {
+  const { classes } = props;
+
+  return (
+    <div className={`${classes.gbContainerLogin}`} justify="center" alignItems="center" >
+
+      <Avatar alt="Logo" src={imgDestaque} className={`${classes.bigAvatar}`} />
+
+      <Button variant="contained" color="primary" className={classes.button}>
+        <Icon className={classes.icon}>games</Icon>Logar com Facebook
+      </Button><br/>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        <Icon className={classes.icon}>games</Icon> Logar com Google
+      </Button>
+      
+    </div>
+  );
 }
 
-export default PagHome;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
